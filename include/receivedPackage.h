c3758ad3d79911165a797dcc98e39c55c47b0e23
Lucas Qu <2267973803@qq.com>
2025-03-18 20:08:31 +0800

    更新接收包裹头文件，添加相关功能声明并修正宏定义

--- include/receivedPackage.h
+++ include/receivedPackage.h
@@ -1,10 +1,13 @@
-#ifndef PACKAGE_H
-#define PACKAGE_H
+#ifndef RECEIVEDPACKAGE_H
+#define RECEIVEDPACKAGE_H
 
 #include "structure.h"
 
 // 包裹相关功能声明
+void addReceivedPackage(struct customer *user);
+void queryReceivedPackages(struct customer *user);
 void freeReceivedPackages(struct package_r *head);
-void freeSentPackages(struct package_s *head);
+void receivedPackagesCreating();
+void displayMenu_receivedPackage();
 
-#endif // PACKAGE_H
+#endif // RECEIVEDPACKAGE_H
